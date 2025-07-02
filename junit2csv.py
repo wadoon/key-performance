2fbdc153e348b81364e5c4cab80266df822768b7
Alexander Weigl <weigl@kit.edu>
2025-07-02 17:39:02 +0200

    junit2csv

--- junit2csv.py
+++ junit2csv.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+import xml.etree.ElementTree as ET
+import sys
+from pathlib import Path
+
+FMT = '"%s", %s","%s",%s'
+
+RUNID = sys.argv[1]
+
+print(FMT % ("run", "classname", "name", "time"))
+for fil in sys.argv[2]:
+    root = ET.parse(fil, ET.XMLParser(encoding="utf8")).getroot()
+    for testcase in root.findall("testsuite"):
+        values = (
+            RUNID,
+            testcase.attrib['classname'],
+            testcase.attrib['name'],
+            testcase.attrib['time'],
+        )
+        print(FMT % values)
